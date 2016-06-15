6628dd420f1e523001b584e1ca595028e6a8724e
Ivan Verhun <ivanver@jfrog.com>
2016-06-15 19:04:18 +0300

    added few updated for pwd, grep and find

--- examples/find/find.sh
+++
@@ -1,2 +0,0 @@
-# find all pom.xml files in current directory and subdirectories, except of 'target' directory
-find . -name pom.xml -not -path "./*/target/*"
--- examples/grep/grep.sh
+++ examples/grep/grep.sh
@@ -6,3 +6,17 @@ grep . yesterday-lyrics.txt
 
 # print only non-empty, non-whitespace-onlylines
 grep -v '^\s*$' yesterday-lyrics.txt
+
+# ==== grep vs egrep
+# prints nothing, because the expression is a literal
+grep "love|lonely" tomorrow-lyrics.txt
+
+# prints all lines containing either love, or lonely
+egrep "love|lonely" tomorrow-lyrics.txt
+
+# search accross directory (try with wc)
+grep -r tomorrow ./*
+grep -i -r tomorrow ./*
+
+# line numbers
+egrep -n "love|lonely" tomorrow-lyrics.txt
