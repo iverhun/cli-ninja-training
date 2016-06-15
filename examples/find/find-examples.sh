ed5e3b1ac2c16fb16c79dcb6f4719e8badd4794a
Ivan Verhun <ivanver@jfrog.com>
2016-06-16 01:43:45 +0300

    more find and pipes examples

--- examples/find/find-examples.sh
+++ examples/find/find-examples.sh
@@ -9,6 +9,7 @@ find . -name "*" -type f -empty -exec ls -lh {} \;
 
 # remove all empty files
 find . -type f -empty  -exec rm -f {} \;
+find . -type f -empty  -delete;
 
 # remove all target directorues: http://www.cyberciti.biz/faq/linux-unix-how-to-find-and-remove-files/
 find . -type d -name "target" -exec rm -rf {} \;
--- examples/pipes/pipes.sh
+++ examples/pipes/pipes.sh
@@ -16,3 +16,16 @@ wc -l < tomorrow-lyrics.txt
 # task
 # emit every 10’s nonempty line in the tomorrow-lyrics.txt file
 cat -b tomorrow-lyrics.txt | grep -e '0\s'
+
+# STDOUT(1) and STDERR(2)
+ll tomorrow-lyrics.txt tomorrow-lyrics.txt2 errors.txt
+ll tomorrow-lyrics.txt tomorrow-lyrics.txt2 > errors.txt
+ll tomorrow-lyrics.txt tomorrow-lyrics.txt2 1> errors.txt
+ll tomorrow-lyrics.txt tomorrow-lyrics.txt2 2> errors.txt
+
+# redirect stderr to stdout: http://ryanstutorials.net/linuxtutorial/piping.php
+# We redirect to a file first then redirect the error stream. 
+# We identify the redirection to a stream by placing an & in front of the stream number 
+# (otherwise it would redirect to a file called 1). 
+ll tomorrow-lyrics.txt tomorrow-lyrics.txt2 > myoutput 2>&1
+
