393d5e2447f49851e7720acc7ada2932dee959d6
Ivan Verhun <ivan.verhun@gmail.com>
2018-10-09 11:26:46 +0300

    added more examples

--- examples/xargs/xargs.sh
+++ examples/xargs/xargs.sh
@@ -3,3 +3,6 @@ ls | xargs cat
 
 # find all usages of 'cat' command in *.sh files in examples directory
 find .. -name "*.sh" | xargs grep 'cat'
+
+grep -rl Jamaika `pwd` | xargs sed -i '' 's/Jamaika/Jamaica/g’
+
