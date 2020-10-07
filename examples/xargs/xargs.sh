28be7dd9a5b9e20f21916a0b2b0d53b89b30aa1a
Ivan Verhun <902581+iverhun@users.noreply.github.com>
2020-10-07 22:57:33 +0300

    added few examples for sed and xargs

--- examples/xargs/xargs.sh
+++ examples/xargs/xargs.sh
@@ -4,5 +4,5 @@ ls | xargs cat
 # find all usages of 'cat' command in *.sh files in examples directory
 find .. -name "*.sh" | xargs grep 'cat'
 
-grep -rl Jamaika `pwd` | xargs sed -i '' 's/Jamaika/Jamaica/g’
+grep -rl Jamaika `pwd` | xargs sed -i '' 's/Jamaika/Jamaica/g'
 
